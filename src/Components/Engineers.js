68894a6d972976d8b75e025c74535538792f6944
Your Name <you@example.com>
2024-06-28 18:19:40 +0300

    add engineer's picturs and edit some padding

--- src/Components/Engineers.js
+++ src/Components/Engineers.js
@@ -4,7 +4,7 @@ import Grid from "@mui/joy/Grid";
 export const Enginners = ({ engs }) => {
   return (
     <>
-      <Grid container p={"20px"} spacing={3} sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3} sx={{ flexGrow: 1 }}>
         {engs.map((eng, index) => (
           <Grid item key={index} xs={12} sm={6} md={3}>
             <EngCard eng={eng} />
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,18 @@ import { Sheet } from "@mui/joy";
 import { DemoVid } from "../Components/DemoVid";
 import { Enginners } from "../Components/Engineers";
 import { NavigateButton } from "../Components/NavigateButton";
+import { AboutProducts } from "../Components/AboutProducts";
+import fahedPic from "../Assets/fahed.jpeg";
+import shadiPic from "../Assets/shadi.jpeg";
+import noorPic from "../Assets/noor.jpeg";
+import shareefPic from "../Assets/shareef.jpeg";
+import ashqarPic from "../Assets/Ashqar.jpeg";
 
 const Container = styled(Sheet)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   background: "var(--bright)",
-  paddingBottom: "50px",
+  padding: "30px",
 }));
 
 export const Home = () => {
@@ -17,39 +23,42 @@ export const Home = () => {
       engName: "م.فهد ياسين",
       engPosition: "المدير العام",
       phoneNum: "+97059 - 7032370",
-      engPic:
-        "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
+      engPic: fahedPic,
     },
     {
-      engName: "م.شريف سعد الدين",
-      engPosition: "مهندس المصانع والصيانة في مجال الالكتروميكانيك ",
-      phoneNum: "+97059-9786696",
-      engPic:
-        "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
-    },
-    {
-      engName: "م.محمد الأشقر",
-      engPosition: "مهندس حلول الطاقة",
-      phoneNum: "+97059 - 7032370",
-      engPic:
-        "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
+      engName: "م.شادي أبو شمعة ",
+      engPosition: "مختص صيانة اليو بي اس وانظمة الباك اب سستم والبطاريات",
+      phoneNum: "+97059-9643478",
+      engPic: shadiPic,
     },
     {
       engName: "م.نور ياسين",
       engPosition: "مهندس المبيعات",
       phoneNum: "+972 59-2458766",
-      engPic:
-        "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
+      engPic: noorPic,
+    },
+    {
+      engName: "م.شريف سعد الدين",
+      engPosition: "مهندس المصانع والصيانة في مجال الالكتروميكانيك ",
+      phoneNum: "+97059-9786696",
+      engPic: shareefPic,
+    },
+    {
+      engName: "م.محمد الأشقر",
+      engPosition: "مهندس حلول الطاقة",
+      phoneNum: "+97059 - 7032370",
+      engPic: ashqarPic,
     },
   ];
   return (
     <>
       <Container>
         <DemoVid />
+        <AboutProducts />
         <Enginners engs={engineers} />
-        <Sheet sx={{ display: "flex", justifyContent: "center" }}>
+        {/* <Sheet sx={{ display: "flex", justifyContent: "center" }}>
           <NavigateButton text={"Submit a Complaint"} to={"/form"} />
-        </Sheet>
+        </Sheet> */}
       </Container>
     </>
   );
